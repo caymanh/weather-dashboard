96ddfbfad731dcd79ec8513c4fb14af4c841da0a
Cayman Heng <hengcayman@gmail.com>
2020-11-05 16:16:37 -0500

    updated function to fetch data from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
-var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
-
-function getForecast() {
-    fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log('Weather Data Here \n----------');
-      for (var i = 0; i < data.length; i++) {
-        console.log(data[i].url);
-        console.log(data[i].user.login);
-      }
-    });
-}
-
-
-$( ".btn" ).on( "click", function() {
+$(document).ready(function () {
+  $(".btn").on("click", function () {
     event.preventDefault();
+    var domain = "https://api.openweathermap.org/data/2.5/forecast?q=";
+    var city = $(".form-control").val();
+    var api = "&appid=97ab95b8a3348a1a4882ff9739694e9c";
+
+    var requestUrl = domain + city + api;
+    function getForecast() {
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+    }
     getForecast();
   });
+});
