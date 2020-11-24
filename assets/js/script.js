a41b775b601f4882f07c1cd8009871f840e63609
Cayman Heng <hengcayman@gmail.com>
2020-11-24 10:58:09 -0500

    added screenshot of web application

--- assets/js/script.js
+++ assets/js/script.js
@@ -216,3 +216,16 @@ $(document).ready(function () {
   };
   displaySearch();
 });
+
+
+var a = 1;
+function outer(){
+	var a = 2;
+	function inner(){
+		a++;
+    console.log(a);
+		var a = 5;
+	}
+	inner();
+}
+outer();
